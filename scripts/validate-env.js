431fe71691afde1d1baaf0c5320c6ae30727e0ac
jaswantgali29-dotcom <jaswantgali29@gmail.com>
2025-11-08 16:31:24 -0800

    Delete scripts/validate-env.js

--- scripts/validate-env.js
+++
@@ -1,9 +0,0 @@
-// simple startup check to avoid forgotten env keys during demo
-const missing = []
-if (!process.env.OPENAI_API_KEY) missing.push('OPENAI_API_KEY')
-if (missing.length) {
-  console.error('Missing env vars:', missing.join(', '))
-  process.exit(1)
-} else {
-  console.log('All required env vars present.')
-}

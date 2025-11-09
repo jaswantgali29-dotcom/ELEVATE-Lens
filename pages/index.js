1e5135a7168e06e9962a35d865e3ee5d2e6071fc
jaswantgali29-dotcom <jaswantgali29@gmail.com>
2025-11-08 16:50:04 -0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AREngine from '../components/AREngine';
+
+export default function Home() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", margin: "20px" }}>ELEVATE Lens</h1>
+      <AREngine />
+    </div>
+  );
+}
